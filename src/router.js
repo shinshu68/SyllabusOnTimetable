3e0a4544aaeab8a427b00cd74ba5c782ebdfacfd
shinshu68 <r_avsr_r68whitemeet_70@yahoo.co.jp>
2019-04-06 16:10:34 +0900

    [update] ルート定義を変更

--- src/router.js
+++ src/router.js
@@ -13,13 +13,28 @@ export default new Router({
       name: 'home',
       component: Home
     },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/:school',
+      name: 'school',
+      component: () => import('./views/School.vue')
+    },
+    {
+      path: '/:school/:year',
+      name: 'year',
+      component: () => import('./views/Year.vue')
+    },
+    {
+      path: '/:school/:year/:class',
+      name: 'class',
+      component: () => import(`./views/Class.vue`)
     }
   ]
 })
