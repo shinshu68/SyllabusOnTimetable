258d29a5602f4d7a259f7a3093e21ca1cd424dfc
shinshu68 <r_avsr_r68whitemeet_70@yahoo.co.jp>
2019-04-06 16:10:06 +0900

    [add] クラスの時間割とシラバスの情報を入れるコンポーネントを追加

---
+++ src/components/Timetable.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="timetable">
+    <!-- {{ data }} -->
+    <ul v-for="day in Object.keys(formatData)" :key="day">
+      <p>{{day}}<br></p>
+      <grid-layout
+            :layout.sync="timeData"
+            :col-num="5"
+            :row-height="30"
+            :is-draggable="false"
+            :is-resizable="false"
+            :is-mirrored="false"
+            :vertical-compact="true"
+            :margin="[1, 1]"
+            :use-css-transforms="false">
+        <grid-item v-for="item in timeData"
+                  :x="item.x"
+                  :y="item.y"
+                  :w="item.w"
+                  :h="item.h"
+                  :i="item.i"
+                  :key="item.i">
+          {{item.i}}
+        </grid-item>
+      </grid-layout>
+      <grid-layout
+            :layout.sync="formatData[day]"
+            :col-num="5"
+            :row-height="150"
+            :is-draggable="false"
+            :is-resizable="false"
+            :is-mirrored="false"
+            :vertical-compact="true"
+            :margin="[1, 1]"
+            :use-css-transforms="false">
+        <grid-item v-for="item in formatData[day]"
+                  :x="item.x"
+                  :y="item.y"
+                  :w="item.w"
+                  :h="item.h"
+                  :i="item.i"
+                  :key="item.id"
+                  :style="{background: item.detail[3]}"
+                  >
+          <span>
+            <p @click="jump(item.url)">{{item["detail"][0]}}</p>
+            <p @click="jump(item.url)">{{item["detail"][1]}}</p>
+            <p @click="jump(item.url)">{{item["detail"][2]}}</p>
+          </span>
+        </grid-item>
+      </grid-layout>
+    </ul>
+  </div>
+</template>
+
+<script>
+import VueGridLayout from 'vue-grid-layout'
+export default {
+  name: "timetable",
+  components: {
+    GridLayout: VueGridLayout.GridLayout,
+    GridItem: VueGridLayout.GridItem
+  },
+  props: {
+    data: Object,
+    schoolID: Number,
+    departmentID: Number
+  },
+  data() {
+    return {
+      formatData: {},
+      timeData: [
+        {"x": 0, "y": 0, "w":1, "h":1, "i":"1 2"},
+        {"x": 1, "y": 0, "w":1, "h":1, "i":"3 4"},
+        {"x": 2, "y": 0, "w":1, "h":1, "i":"5 6"},
+        {"x": 3, "y": 0, "w":1, "h":1, "i":"7 8"},
+        {"x": 4, "y": 0, "w":1, "h":1, "i":"9 10"}
+      ]
+    }
+  },
+  mounted() {
+    let tmpArray = []
+    let x = 0
+    let y = 0
+    let h = 1
+    let w = 0
+    Object.keys(this.data).forEach(day => {
+      tmpArray = []
+      x = 0
+      this.data[day].forEach((subject, index) => {
+        let nums = Object.keys(subject)[0].split(' ')
+        let start = Number(nums[0])
+        let end = Number(nums[1])
+        w = (end - start + 1) / 2
+        let obj = this.data[day][index][Object.keys(subject)[0]]
+        let i = ""
+        let detail = []
+        let url = ""
+        if (obj) {
+          i = obj["name"]
+          detail.push(obj["name"])
+          detail.push(obj["teacher"])
+          detail.push(obj["place"])
+          if (obj["compulsory"]) {
+            detail.push('orange')
+          } else {
+            detail.push('cyan')
+          }
+          url = obj['url']
+        } else {
+          detail.push(null)
+          detail.push(null)
+          detail.push(null)
+          detail.push('')
+        }
+        let subjectData = {"id": `${x}${y}${w}${h}`, "x": x, "y": y, "w": w, "h": h, "i": i, "detail": detail, "url": url}
+        x += w
+        tmpArray.push(subjectData)
+      })
+      this.$set(this.formatData, day, tmpArray)
+      y += 1
+    });
+  },
+  methods: {
+    jump(url) {
+      let baseurl = 'https://syllabus.kosen-k.go.jp/Pages/PublicSyllabus'
+      location.href = baseurl + `?school_id=${this.schoolID}&department_id=${this.departmentID}&` + url
+      // console.log(baseurl + url)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+p {
+  margin-block-start: 0.125rem;
+  margin-block-end: 0.125rem;
+  font-size: 16px;
+  vertical-align: middle;
+}
+
+span {
+  vertical-align: middle;
+}
+
+div.vue-grid-item {
+  border: 1px solid #000000;
+  display: inline-block;
+  width: calc(100vw / 5);
+}
+</style>
+
+
---
+++ src/components/toyota/2019/1J/ClassData.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="classdata">
+    <div id="nav">
+      <span @click="mode='first'">前期</span> |
+      <span @click="mode='second'">後期</span>
+    </div>
+    <Timetable :data="FirstTimetable" :schoolID="schoolID" :departmentID="departmentID" v-if="mode=='first'"/>
+    <Timetable :data="SecondTimetable" :schoolID="schoolID" :departmentID="departmentID" v-else-if="mode=='second'"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Timetable from '@/components/Timetable.vue'
+
+export default {
+  name: 'classdata',
+  components: {
+    Timetable
+  },
+  data() {
+    return {
+      schoolID: 23,
+      departmentID: 25,
+      FirstTimetable: {
+        "Mon": [
+          { "1 2": {
+            "name": "歴史学",         // 教科名
+            "teacher": "京極",        // 教師名
+            "place": "S22",           // 場所
+            "concurrently": false,    // 並行開講かどうか
+            "compulsory": true,	      // 必修かどうか
+            "url": "subject_code=90015&year=2014"
+          }},
+          { "3 4": {
+            "name": "解析力学",
+            "teacher": "榎本",
+            "place": "S22",
+            "concurrently": false,
+            "compulsory": false,
+            "url": "subject_code=91011&year=2014"
+          }},
+          { "5 6": {
+            "name": "応用情報システム",
+            "teacher": "藤原賢",
+            "place": "情報教室(5)",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=95028&year=2014"
+          }},
+          { "7 8": null },
+          { "9 10": null }
+        ],
+        "Tue": [
+          { "1 2": {
+            "name": "生物化学",
+            "teacher": "三浦",
+            "place": "S22",
+            "concurrently": false,
+            "compulsory": false,
+            "url": "subject_code=91018&year=2014"
+          }},
+          { "3 4": {
+            "name": "ソフトウェア工学",
+            "teacher": "木村",
+            "place": "情報教室(5)",
+            "concurrently": false,
+            "compulsory": false,
+            "url": "subject_code=95013&year=2014"
+          }},
+          { "5 6": null },
+          { "7 8": null },
+          { "9 10": null }
+        ],
+        "Wed": [
+          { "1 8": {
+            "name": "情報科学実験",
+            "teacher": "稲垣",
+            "place": "テクノセンタ",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=95001&year=2013"
+          }},
+          { "9 10": null }
+        ],
+        "Thu": [
+          { "1 2": {
+            "name": "技術者倫理",
+            "teacher": "北野",
+            "place": "S22",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=90013&year=2014"
+          }},
+          { "3 8": {
+            "name": "特別研究",
+            "teacher": "担当教員",
+            "place": "",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=95302&year=2014"
+          }},
+          { "9 10": null }
+        ],
+        "Fri": [
+          { "1 2": {
+            "name": "線形代数学",
+            "teacher": "吉澤",
+            "place": "S22",
+            "concurrently": false,
+            "compulsory": false,
+            "url": "subject_code=91012&year=2014"
+          }},
+          { "3 4": {
+            "name": "総合英語I",
+            "teacher": "市川",
+            "place": "S22 MM2",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=90011&year=2014"
+          }},
+          { "5 6": {
+            "name": "コンピュータシステム",
+            "teacher": "稲垣",
+            "place": "情報教室(5)",
+            "concurrently": false,
+            "compulsory": false,
+            "url": "subject_code=95011&year=2014"
+          }},
+          { "7 8": {
+            "name": "論理回路設計",
+            "teacher": "仲野",
+            "place": "情報教室(5)",
+            "concurrently": false,
+            "compulsory": true,
+            "url": "subject_code=95017&year=2014"
+          }},
+          { "9 10": null }
+        ]
+      },
+      mode: "first"
+    }
+  }
+}
+</script>
