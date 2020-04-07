94702875c0d861bbc121b501ca08417999757d0f
shinshu68 <shinshu68@gmail.com>
2020-04-07 12:10:04 +0900

    :sparkles: Rebuild

---
+++ docs/precache-manifest.b8501211fe8d434bc71dc31f97676545.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6145fadd6e84c8bf956",
+    "url": "/SyllabusOnTimetable/css/app.14663064.css"
+  },
+  {
+    "revision": "68c73f6182023c506128",
+    "url": "/SyllabusOnTimetable/css/chunk-6ee4d551.191afb25.css"
+  },
+  {
+    "revision": "b372308ef4b4b4cff2461798495b4fbc",
+    "url": "/SyllabusOnTimetable/index.html"
+  },
+  {
+    "revision": "a6145fadd6e84c8bf956",
+    "url": "/SyllabusOnTimetable/js/app.e97d4e5c.js"
+  },
+  {
+    "revision": "4b1a5b3e49e5eb94b39f",
+    "url": "/SyllabusOnTimetable/js/chunk-2d0c1b88.27924a62.js"
+  },
+  {
+    "revision": "b8e848294fc63d3de63d",
+    "url": "/SyllabusOnTimetable/js/chunk-2d23860a.ac3f9603.js"
+  },
+  {
+    "revision": "68c73f6182023c506128",
+    "url": "/SyllabusOnTimetable/js/chunk-6ee4d551.2343d888.js"
+  },
+  {
+    "revision": "9d4803230c76a4354b03",
+    "url": "/SyllabusOnTimetable/js/chunk-vendors.45290522.js"
+  },
+  {
+    "revision": "1352ffc5533401b9bb38056a85b901c3",
+    "url": "/SyllabusOnTimetable/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SyllabusOnTimetable/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SyllabusOnTimetable/precache-manifest.0f79c75a4cd10061c25ae756fd618057.js"
+  "/SyllabusOnTimetable/precache-manifest.b8501211fe8d434bc71dc31f97676545.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "syllbustable"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "syllbustable"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
